0a3abafd676130ac3598ba63cb9128d30a7def7c
irere123 <irere2020@outlook.com>
2025-04-19 13:58:49 +0200

    feat(web): add landing pages footer section

--- apps/web/src/components/marketing/faq.tsx
+++ apps/web/src/components/marketing/faq.tsx
@@ -1,3 +1,9 @@
 export function FAQ() {
-  return <div>FAQ</div>;
+  return (
+    <div className="mt-8 max-w-[390px] lg:max-w-6xl @container px-[var(--page-px)] w-full mx-auto flex flex-col items-center">
+      <div className="flex flex-col items-center">
+        <p>Hello</p>
+      </div>
+    </div>
+  );
 }
